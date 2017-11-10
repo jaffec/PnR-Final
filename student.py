8e0a25d3e60a1bb60193d2068079409eb587a5ad
jaffec <jaffec@gilmour.org>
2017-11-10 14:37:27 -0500

    Changed direction layout and added an escape if there is nowhere to go

--- student.py
+++ student.py
@@ -1,5 +1,6 @@
 import pigo
 import time  # import just in case students need
+import datetime
 import random
 
 # setup logs
@@ -16,6 +17,9 @@ class Piggy(pigo.Pigo):
     def __init__(self):
         """The robot's constructor: sets variables and runs menu loop"""
         print("I have been instantiated!")
+        self.start_time = datetime.datetime.utcnow()
+
+
         # Our servo turns the sensor. What angle of the servo( ) method sets it straight?
         self.MIDPOINT = 106
         # YOU DECIDE: How close can an object get (cm) before we have to stop?
@@ -144,6 +148,10 @@ class Piggy(pigo.Pigo):
         print("-----------! NAVIGATION ACTIVATED !------------\n")
         print("-------- [ Press CTRL + C to stop me ] --------\n")
         print("-----------! NAVIGATION ACTIVATED !------------\n")
+        rightnow = datetime.datetime.utcnow()
+        difference = (right_now - self.start_time).seconds
+        print ("It took you %d seconds to run this" % rightnow - self.start_time)
+
         while True:
             if self.is_clear():
                 self.cruise()
@@ -158,6 +166,20 @@ class Piggy(pigo.Pigo):
                     #check right and go right if clear
 
                     #look left 2 times and then go
+    def smooth_turn(self):
+        self.right_rot()
+        start - datetime.datetime.utcnow()
+        self.servo(self.MIDPOINT)
+        while true:
+            if self.dist() > 100:
+                self.stop()
+                    print("I think ive found a good path")
+            elif datetime.datetime.utcnow() - start > datetime.timedelta(seconds = 10)
+                self.stop()
+                    print()
+            time.sleep(.2)
+
+
     def cruise(self):
         """ drive straight while path is clear """
         self.fwd()
